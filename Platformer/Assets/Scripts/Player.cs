a892157fe3f01aaa91526dd82cd5b87fdd98efef
RevenantBane <kenbob.21@gmail.com>
2021-09-19 14:48:37 -0700

    -Fixed Character's Sprite Direction

--- Platformer/Assets/Scripts/Player.cs
+++ Platformer/Assets/Scripts/Player.cs
@@ -20,8 +20,16 @@ public class Player : MonoBehaviour
         //Adding a dollar sign allows to place variables in quotes
         Debug.Log($"Velocity = {rigidbody2D.velocity}");
 
+        //Walk animation transition status
         var animator = GetComponent<Animator>();
         bool walking = horizontal != 0;
         animator.SetBool("Walk", walking);
+
+        if (horizontal != 0)
+        {
+            //Turn Player animation to other direction
+            var spriteRenderer = GetComponent<SpriteRenderer>();
+            spriteRenderer.flipX = horizontal < 0;
+        }
     }
 }
