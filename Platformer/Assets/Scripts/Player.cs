ae992108b75306ded6d4a61c07f9deb94ba39659
RevenantBane <kenbob.21@gmail.com>
2021-09-19 06:45:11 -0700

    Platformer Player Movement Added

---
+++ Platformer/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveHorizontal();
+    }
+
+    private void MoveHorizontal()
+    {
+        var horizontal = Input.GetAxis("Horizontal");
+        var rigidbody2D = GetComponent<Rigidbody2D>();
+        rigidbody2D.velocity = new Vector2(horizontal, rigidbody2D.velocity.y);
+    }
+}
